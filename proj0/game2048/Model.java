b600bc4be877215f20f85a3101ebbd61adbfd2de
FalKon <kevinchu6601@gmail.com>
2022-02-16 22:08:24 +0800

    done with proj0

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author TODO: FalKon1256
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -114,7 +114,102 @@ public class Model extends Observable {
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
 
+        // Check for null input.
+        if (side == null || board == null) {
+            return false;
+        }
+
+        // Initialize trace parameters.
+        int topMovablePos = size() - 1;
+        int prevTileValue = 0;
+        boolean merged = false;
+
+        // Set for which direction the tile will move
+        board.setViewingPerspective(side);
+
+        /** Iterate over each column.
+         *  Check the rules for each tile in the columns.
+         *  Move the tile if the rules matches.
+         */
+        for (int col = 0; col < size(); col++) {
+            for (int row = size() - 1; row >= 0; row--) {
+
+                // Set t as the current tile.
+                Tile t = board.tile(col, row);
+
+                // Check if tile is not empty.
+                if (t != null) {
+
+                    // There's at least 1 tile with value above.
+                    if (prevTileValue != 0) {
+                        /** Value is same as the top movable position tile:
+                         *  Initial state for merge must be false.
+                         *  The tile must move.
+                         *
+                         *  1. Move the tile to top movable position.
+                         *  2. Set changed state to true.
+                         *  3. Check if the tile merges (or not) after this move.
+                         *     If tile merges:
+                         *     1. Update score.
+                         *     2. Top movable position - 1.
+                         */
+                        if (t.value() == prevTileValue) {
+                            merged = board.move(col, topMovablePos, t);
+                            changed = true;
+                            if (merged) {
+                                score += board.tile(col, topMovablePos).value();
+                                topMovablePos--;
+                            }
+                        } else {
+                            /**
+                             *  Value is different from the top movable position tile:
+                             *  1. Record the tile value.
+                             *  2. Check if the previous tile have merged.
+                             *     If merged:
+                             *     1. top movable position - 1
+                             *  3. Move the tile to top movable position.
+                             *  4. Check if the tile moves.
+                             *     If tile moves:
+                             *     1. Set changed state to true
+                             */
+                            prevTileValue = t.value();
+                            if (!merged) {
+                                topMovablePos--;
+                            }
+                            merged = board.move(col, topMovablePos, t);
+                            if (row != topMovablePos) {
+                                changed = true;
+                            }
+                        }
+                    } else {
+                        /** There's no tile with value above.
+                         *  All tiles above are empty or At the top tile of the column:
+                         *  1. Record the tile value.
+                         *  2. Check if top tile is empty
+                         *     If top tile is empty:
+                         *     1. Move the tile to top.
+                         *     2. Set changed state to true
+                         */
+                        prevTileValue = t.value();
+                        if (board.tile(col, topMovablePos) == null) {
+                            merged = board.move(col, topMovablePos, t);
+                            changed = true;
+                        }
+                    }
+                }
+            }
+
+            // Reset trace parameters for next column.
+            topMovablePos = size() - 1;
+            prevTileValue = 0;
+            merged = false;
+        }
+
         checkGameOver();
+
+        // Set the observing direction back to north
+        board.setViewingPerspective(Side.NORTH);
+
         if (changed) {
             setChanged();
         }
@@ -138,6 +233,26 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+
+        // Check for null input.
+        if (b == null) {
+            return false;
+        }
+        /** Must implement Iterable & Iterator
+         *  to create AllTileIterator class object.
+         *  This will override at run-time and return Tile class object.
+         *  Note: This will work as well.
+         *      for (int col = 0; col < b.size(); col++) {
+         *          for (int row = 0; row < b.size(); row++) {
+         *              ...
+         *          }
+         *      }
+         */
+        for (Tile t : b) {
+            if (t == null) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -148,6 +263,21 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+
+        // Check for null input.
+        if (b == null) {
+            return false;
+        }
+
+        /** Iterate over the 2D array.
+         *  (Tile[][] attribute in the input variable).
+         *  Must check first if tile is null.
+         */
+        for (Tile t : b) {
+            if (t != null && t.value() == MAX_PIECE) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -159,6 +289,50 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+
+        // Check for null input.
+        if (b == null) {
+            return false;
+        }
+
+        /** From col0 to col2 & row0 to row2, check the rules as follows:
+         *  1. Check for at least one empty space.
+         *  2. Check for two adjacent tiles with the same value.
+         */
+
+        // Check rule 1 for start tile (0,0).
+        if (b.tile(0, 0) == null) {
+            return true;
+        }
+
+        for (int col = 0; col < b.size() - 1; col++) {
+            for (int row = 0; row < b.size() - 1; row++) {
+                // Check rule 1.
+                if (b.tile(col + 1, row) == null || b.tile(col, row + 1) == null) {
+                    return true;
+                }
+                // Check rule 2.
+                if (b.tile(col, row).value() == b.tile(col + 1, row).value()
+                    || b.tile(col, row).value() == b.tile(col, row + 1).value()) {
+                    return true;
+                }
+            }
+        }
+
+        // Check rule 1 for the remaining unchecked tile (3,3).
+        if (b.tile(3, 3) == null) {
+            return true;
+        }
+
+        // Check last col & row for the rule 2.
+        for (int i = 0; i < b.size() - 1; i++) {
+            if (b.tile(3, i).value() == b.tile(3, i + 1).value()) {
+                return true;
+            }
+            if (b.tile(i, 3).value() == b.tile(i + 1, 3).value()) {
+                return true;
+            }
+        }
         return false;
     }
 
